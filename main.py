99ab8ac32a05a0e55f7f06907698648624d1fdd8
Haster <20924663+migbash@users.noreply.github.com>
2019-03-13 03:13:49 +0000

    Implemented AJAX forms, Flask-MongoDB connection, Flask-Validation Methods, Responsive CSS cross-platform, AJAX-POST-GET-Error Handling, and other minor touches, approaching the end of full-stack.

--- main.py
+++ main.py
@@ -1,13 +1,28 @@
-# !flask/bin/Python
-
-from flask import Flask, render_template
+from flask import Flask, render_template, request, jsonify
+from flask_pymongo import PyMongo
+import dns
 
 app = Flask(__name__)
+app.config['MONGO_DBNAME'] = 'enubfs_data'
+app.config['MONGO_URI'] = 'mongodb+srv://mhashX:EeAuDKtDeUZV8x00@enubfs-ak8by.mongodb.net/enubfs_data'
+mongo = PyMongo(app)
 
 # ------- Main Page -------
 @app.route('/')
 def index():
     return render_template('index.html')
 
+# ------- Sending Subscription User Data to MongoDB Atlas -------
+@app.route('/subscribed', methods=['GET','POST'])
+def subscribed():
+    email_txt = request.form['email']
+    target_db = mongo.db.email_subscriptions
+    email_check = target_db.find_one({'email': email_txt})          # Email Validation
+    if email_check:
+        return jsonify({'error':'Uh-oh..email already in use'})     # Email Duplicate Error Message
+    else:
+        target_db.insert({'email': email_txt})
+        return jsonify({'success':'Success! Your are Subscribed'})  # Email Correct
+
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True, port=8080)
--- static/main.js
+++ static/main.js
@@ -1,21 +1,39 @@
-//Fade In Website
+// ---- Fade In Website ----
 
 $(document).ready(function() {
-  $('section').hide().fadeIn(1500);
+  $('#landing-page, footer').hide();
+  $('#landing-page').fadeIn(3000 , function() {
+    $('footer').fadeIn(1500);
+  });
 });
 
-//Window = Section Size//
-
+// ---- AJAX POST Function (Non-Reloding / Non-Refreshing) ----
 $(document).ready(function() {
-  function setHeight() {
-    windowHeight = $(window).innerHeight();
-    windowWidth = $(window).width();
-    $('section').css('min-height', windowHeight);
-    $('section').css('width', w);
-  };
-  setHeight();
-
-  $(window).resize(function() {
-    setHeight();
+  $('form').on('submit', function(event) {
+      $.ajax({
+          url: '/subscribed',
+          type: 'POST',
+          data: {
+              email : $('#input').val()
+          }
+      })
+      .done(function(data){
+        // ---- Validating Input ----
+        if(data.error) {
+          // Error Message
+          $('#error-box').addClass("error").text(data.error).animate({left:'0px', width: 'toggle', opacity: 'toggle'}, 500, function(){
+          $('#error-box').delay(3000).animate({width: 'toggle', opacity: 'toggle'}, 300);
+          });
+        } else {
+          // Success Message
+          $('#error-box').addClass("success").text(data.success).animate({left:'0px', width: 'toggle', opacity: 'toggle'}, 500, function(){
+          $('#error-box').delay(3000).animate({width: 'toggle', opacity: 'toggle'}, 300);
+          });
+        }
+      });
+      // Remove Class after Use
+      $('#error-box').removeClass("error");
+      $('#error-box').removeClass("success");
+      event.preventDefault();
   });
 });
